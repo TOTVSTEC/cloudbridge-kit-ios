ae93a0045e120935d531da0e76e3bd07aa2321ce
Daniel Otto Bolognani <daniel.bolognani@totvs.com.br>
2017-03-14 09:22:50 -0300

    Incluindo controle de versao

---
+++ update.js
@@ -0,0 +1,18 @@
+ar task = module.exports,
+   Q = null;
+
+task.run = function run(cli, targetPath, projectData) {
+  Q = cli.require('q');
+
+  return Q()
+    .then(function() {
+      var remove = require('./remove');
+
+      return remove.run(cli, targetPath, projectData);
+    })
+  .then(function() {
+    var install = require('./install');
+
+    return install.run(cli, targetPath, projectData);
+  });
+};
