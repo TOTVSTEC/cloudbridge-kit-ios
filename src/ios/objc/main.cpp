4059f4787a963b3df4e0dee6834b7344d1acaf0c
Daniel Otto Bolognani <daniel.bolognani@totvs.com.br>
2017-07-11 15:58:28 -0300

    Alterando projeto e binario para correto funcionamento do GPS no iOS

---
+++ src/ios/objc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cloudbridgeapp
+//
+//  Created by Daniel Bolognani on 11/07/17.
+//  Copyright © 2017. All rights reserved.
+//
+//
+#import "cloudbridge.h"
+
+int main(int argc, char *argv[])
+{
+    CloudBridge(argc, argv);
+}
